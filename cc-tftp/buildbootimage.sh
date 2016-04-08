2f64b2db16825c9c28a44137311ae7c038fdae69
housek <housenkobe@gmail.com>
2016-04-08 07:37:07 +0200

    tidy up

--- cc-tftp/buildbootimage.sh
+++ cc-tftp/buildbootimage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 grub2-mkstandalone			\
 	--compress=gz			\
-	-d /usr/lib/grub/x86_64-efi/	\
+	-d /usr/lib/grub2/x86_64-efi/	\
 	-O x86_64-efi			\
 	--fonts="unicode"		\
 	-o grub2.efi			\
