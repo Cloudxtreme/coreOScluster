b59cc0397397015fd311da34c71641a36fcecaed
housek <housenkobe@gmail.com>
2016-04-12 19:39:34 +0200

    Clean file structure and more efficient building script

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,13 +10,20 @@ cp cc-nginx/pxe-cloud-config.yml.template cc-nginx/pxe-cloud-config.yml
 discoveryUrl=`scripts/newDiscoveryToken.sh`
 sed -i "s,<discoveryTokenUrlHere>,$discoveryUrl,g" cc-nginx/pxe-cloud-config.yml
 
+#build ipxe bootfile
+scripts/buildBootImage.sh 			> /dev/null &
+bibpid=$!
 
 
 #step two build docker images
 docker build -t housek/cc-dhcp cc-dhcp		> /dev/null &
-docker build -t housek/cc-tftp cc-tftp		> /dev/null &
 docker build -t housek/cc-nginx cc-nginx	> /dev/null &
 echo "Building all Docker images (This could take a long time) please wait..."
+echo "Waiting for bootimage to complete building before starting tftp server build..."
+wait $bibpid
+echo "Started building tftp server..."
+docker build -t housek/cc-tftp cc-tftp		> /dev/null &
+echo "Waiting for all remaining processes to end..."
 wait
 echo "All docker images build. We are done."
 exit 0
--- scripts/buildBootImage.sh
+++ scripts/buildBootImage.sh
@@ -2,5 +2,5 @@
 set -e
 docker build -t housek/cc-bib cc-bib
 docker run --rm --volume ` pwd`/cc-bib/output:/output housek/cc-bib
-mv -r cc-bib/output/* cc-tftp
+mv cc-bib/output/* cc-tftp
 exit 0
