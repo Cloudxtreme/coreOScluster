16608e1c43a65180694ef41b7c599ae6ccc4b229
Kobe Housen <kobe@localhost.localdomain>
2016-04-21 09:27:35 +0200

    working boot from source webserver

--- scripts/buildBootImage.sh
+++ scripts/buildBootImage.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
+
+
 docker build -t housek/cc-bib cc-bib
-docker run --rm --volume ` pwd`/cc-bib/output:/output housek/cc-bib
-mv cc-bib/output/* cc-tftp
+ID=`docker create housek/cc-bib`
+docker cp $ID:ipxe/src/bin-x86_64-efi/ipxe.efi cc-tftp
+docker rm $ID
 exit 0
